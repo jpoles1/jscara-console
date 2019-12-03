710e802d9d38e41b38967ee85c93372b55978b7c
Jordan Poles <jpdev.noreply@gmail.com>
2019-12-02 20:01:40 -0500

    Warning lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
 		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 		"no-constant-condition": "off",
-		"prettier/prettier": ["error", { "useTabs": true, "printWidth": 140, "trailingComma": "es5" }],
+		"prettier/prettier": ["warning", { "useTabs": true, "printWidth": 140, "trailingComma": "es5" }],
 	},
   parserOptions: {
     parser: "@typescript-eslint/parser"
