0d99c38376fe2ef308642494fcb7e4c5949ef012
Jordan Poles <jpdev.noreply@gmail.com>
2020-12-04 11:35:43 -0500

    Release 1.0.0

---
+++ dist/precache-manifest.ab1ba43a818fac95c3e99c79ad1216e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364d6719c446c1829e58",
+    "url": "/css/app.a36202cd.css"
+  },
+  {
+    "revision": "3ed0063161dd36b63861",
+    "url": "/css/chunk-vendors.765d869d.css"
+  },
+  {
+    "revision": "2d5c612e2e24320f76d7cf59fb83a093",
+    "url": "/fonts/ChunkFive-Regular.otf"
+  },
+  {
+    "revision": "5bf06596a053153248631d74f9fc4e28",
+    "url": "/fonts/GreatVibes-Regular.ttf"
+  },
+  {
+    "revision": "d0ee52acadb936a2f5a7584732823e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "364d6719c446c1829e58",
+    "url": "/js/app.23186ddf.js"
+  },
+  {
+    "revision": "3ed0063161dd36b63861",
+    "url": "/js/chunk-vendors.8e2442c7.js"
+  },
+  {
+    "revision": "d27606b5ae573e0e29465496b6fbfab5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
